4ad719c2e6460a3f34ebbb44f597c943d3cde5d9
Lezly Agsunod <cpagsunod@me.com>
2015-12-09 18:59:52 -0600

    Initial Commit.

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Did I get git to work?");
+
+	}
+
+}
